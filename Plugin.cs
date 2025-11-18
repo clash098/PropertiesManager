9bf79dd024ac330b0f2b97c4ad06532eb669e757
clash <calanjmooney@icloud.com>
2025-11-18 22:33:00 +1100

    Refactor Plugin.cs for command registration and properties

--- Plugin.cs
+++ Plugin.cs
@@ -1,61 +1,136 @@
+using System.Text;
 using BepInEx;
 using BepInEx.Configuration;
+using Photon.Pun;
+using ExitGames.Client.Photon;
 using ComputerInterface.Behaviours;
 using ComputerInterface.Interfaces;
-using ExitGames.Client.Photon;
-using Photon.Pun;
-using System.Linq;
+
 
 namespace PropertiesManager
 {
-    [BepInPlugin("com.uhclash.gorillatag.propsmanager", "PropertiesManager", "1.0.0")]
+    [BepInPlugin("com.uhclash.gorillatag.PropertiesManager", "PropertiesManager", "1.0.0")]
     [BepInDependency("tonimacaroni.computerinterface", "1.8.0")]
     public class Plugin : BaseUnityPlugin
     {
-        static ConfigEntry<bool> NukeConfig;
-        static readonly ConfigDefinition NukeSetting = new("Property Nuker", "Enabled");
-        public static bool NukeEnabled
+        public static bool NukeEnabled = false;
+        private static ConfigEntry<bool> _nukeEnabledConfig;
+
+        void Awake()
         {
-            get => NukeConfig.Value;
-            set => NukeConfig.Value = value;
+            _nukeEnabledConfig = Config.Bind("Settings", "NukeEnabled", false, "Is the property nuke enabled?");
+            NukeEnabled = _nukeEnabledConfig.Value;
         }
-        void Awake() => NukeConfig = Config.Bind(NukeSetting, false, new ConfigDescription($"Should PropertiesManager disable properties all together?"));
-        void Start()
+
+        void OnEnable()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+            new PropertiesCommandManager().Initialize();
+        }
+
+        void OnDisable()
+        {
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+
+        public static void SaveNukeState()
+        {
+            _nukeEnabledConfig.Value = NukeEnabled;
+        }
+    }
+
+    public class PropertiesCommandManager : ICommandRegistrar
+    {
+        public void Initialize()
+        {
+            RegisterCommands();
+        }
+
+        public void RegisterCommands()
         {
             var commandHandler = CommandHandler.Singleton;
-
+            
             commandHandler.AddCommand(new Command("myprops", null, args =>
             {
-                return $"Properties:\n\n{string.Join('\n', PhotonNetwork.LocalPlayer.CustomProperties.Select(P => $"{P.Key}: {P.Value}"))}";
-            }));
-
-            commandHandler.AddCommand(new Command("clearprops", null, args =>
-            {
-                var props = PhotonNetwork.LocalPlayer.CustomProperties;
-                if (props.Count == 0) return "You have no properties to clear!";
-
-                var propsToRemove = new Hashtable();
-                foreach (var key in props.Keys)
+                if (!PhotonNetwork.InRoom)
                 {
-                    if (key is not string str || str != "didTutorial")
+                    return "You're not currently in a room!";
+                }
+
+                var localPlayer = PhotonNetwork.LocalPlayer;
+                var stringBuilder = new StringBuilder();
+    
+                stringBuilder.AppendLine($"Your Properties ({localPlayer.NickName}):\n");
+
+                if (localPlayer.CustomProperties.Count == 0)
+                {
+                    stringBuilder.AppendLine("You have no custom properties!");
+                }
+                else
+                {
+                    int count = 0;
+                    foreach (var prop in localPlayer.CustomProperties)
                     {
-                        propsToRemove[key] = null;
+                        stringBuilder.Append($"{prop.Key}");
+                        count++;
+
+                        if (count < localPlayer.CustomProperties.Count)
+                        {
+                            stringBuilder.Append(", ");
+                        }
+
+                        if (count % 4 == 0 && count < localPlayer.CustomProperties.Count)
+                        {
+                            stringBuilder.AppendLine();
+                        }
+                    }
+
+                    if (count % 4 != 0)
+                    {
+                        stringBuilder.AppendLine();
                     }
                 }
 
-                PhotonNetwork.LocalPlayer.SetCustomProperties(propsToRemove);
-                return $"Cleared {props.Count} total props!";
+                return stringBuilder.ToString();
+            }));
+            
+            commandHandler.AddCommand(new Command("clearprops", null, args =>
+            {
+                if (!PhotonNetwork.InRoom)
+                {
+                    return "Not in a room!";
+                }
+
+                var localPlayer = PhotonNetwork.LocalPlayer;
+
+                if (localPlayer.CustomProperties.Count == 0)
+                {
+                    return "You have no properties to clear!";
+                }
+
+                var propsToRemove = new Hashtable();
+                foreach (var key in localPlayer.CustomProperties.Keys)
+                {
+                    propsToRemove[key] = null;
+                }
+
+                localPlayer.SetCustomProperties(propsToRemove);
+
+                return $"Cleared {propsToRemove.Count} properties!";
             }));
 
-            commandHandler.AddCommand(new Command("propnuke", [typeof(bool)], args =>
+            commandHandler.AddCommand(new Command("togglepropnuke", null, args =>
             {
-                if (args.Length == 0 || args[0] is not bool enabled) return "An unexpected error occured.";
+                Plugin.NukeEnabled = !Plugin.NukeEnabled;
+                Plugin.SaveNukeState();
 
-                Plugin.NukeEnabled = enabled;
-                return enabled ? "The property nuke has been activated!" : "The property nuke has been disabled!";
+                string status = Plugin.NukeEnabled ? "ENABLED" : "DISABLED";
+                string description = Plugin.NukeEnabled
+                    ? "All custom properties are now being blocked!"
+                    : "All custom properties are now allowed!";
+
+                return $"Property Nuke: {status}\n\n{description}\n(Saved for next restart)";
             }));
         }
-        void OnEnable() => HarmonyPatches.ApplyHarmonyPatches();
-        void OnDisable() => HarmonyPatches.RemoveHarmonyPatches();
     }
 }
